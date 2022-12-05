306568ee62d667451e56e156c15e7e601964912a
Mateus <113111473+schzar@users.noreply.github.com>
2022-12-05 10:41:43 -0300

    Update cron.sh

--- install/cron.sh
+++ install/cron.sh
@@ -1,4 +1,3 @@
-  GNU nano 6.2                                                                  cron.sh                                                                           
 #!/bin/sh
 set -eu
 
